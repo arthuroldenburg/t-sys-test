191bf3921a13ad3798e9999a250d0001c20dc753
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2024-07-03 19:17:55 -0300

    Verificacao dos ultimos dois digitos pronta

---
+++ prova/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        VerificarUltimosDoisDigitos a1 = new VerificarUltimosDoisDigitos();
+        boolean verificacao = a1.verificarUltimosDoisDigitos("cadastro", "cadastro");
+        System.out.println(verificacao);
+    }
+}
